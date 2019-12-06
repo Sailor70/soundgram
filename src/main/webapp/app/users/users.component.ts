83137805e41c8f2b458374995fd1ea95f0d10122
Sailor70 <grzywa96@gmail.com>
2019-12-06 13:50:20 +0100

    users component mods

--- src/main/webapp/app/users/users.component.ts
+++ src/main/webapp/app/users/users.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+import { AccountService } from 'app/core/auth/account.service';
+import { UserService } from 'app/core/user/user.service';
+import { User } from 'app/core/user/user.model';
+import { FollowedUserService } from 'app/entities/followed-user/followed-user.service';
 
 @Component({
   selector: 'jhi-users',
@@ -6,11 +18,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  message: string;
+  currentAccount: any;
+  users: User[];
+  error: any;
+  success: any;
+  userListSubscription: Subscription;
+  routeData: Subscription;
+  links: any;
+  totalItems: any;
+  itemsPerPage: any;
+  page: any;
+  predicate: any;
+  previousPage: any;
+  reverse: any;
 
-  constructor() {
-    this.message = 'UsersComponent message';
+  constructor(
+    private userService: UserService,
+    private alertService: JhiAlertService,
+    private accountService: AccountService,
+    private parseLinks: JhiParseLinks,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private eventManager: JhiEventManager,
+    private modalService: NgbModal,
+    private followedUserService: FollowedUserService
+  ) {
+    this.itemsPerPage = ITEMS_PER_PAGE;
+    this.routeData = this.activatedRoute.data.subscribe(data => {
+      //this.page = data.pagingParams.page;
+      //this.previousPage = data.pagingParams.page;
+      //this.reverse = data.pagingParams.ascending;
+      //this.predicate = data.pagingParams.predicate;
+    });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.accountService.identity().subscribe(account => {
+      this.currentAccount = account;
+      this.loadAll();
+      this.registerChangeInUsers();
+    });
+  }
+
+  transition() {
+    this.router.navigate(['./'], {
+      relativeTo: this.activatedRoute.parent,
+      queryParams: {
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    });
+    this.loadAll();
+  }
+
+  loadPage(page: number) {
+    // if (page !== this.previousPage) {
+    //   this.previousPage = page;
+    //   this.transition();
+    // }
+  }
+
+  loadAll() {
+    this.userService
+      .query({
+        // page: this.page - 1,
+        size: this.itemsPerPage
+        // sort: this.sort()
+      })
+      .subscribe((res: HttpResponse<User[]>) => this.onSuccess(res.body, res.headers), (res: HttpResponse<any>) => this.onError(res.body));
+  }
+
+  followUser(id: number) {
+    this.followedUserService.create(id).subscribe(
+      res => {
+        // eventManager wyświetla alerty na zielono lub czerwono
+        this.eventManager.broadcast({
+          name: 'followedUserListModification',
+          content: 'Follow user with id '
+        });
+        //this.activeModal.dismiss(true);
+      },
+      // jeśli zwraca null to powinien wyświetlić alert że nie można obserwować tego usera
+      res => this.onError(res.body)
+    );
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  registerChangeInUsers() {
+    this.userListSubscription = this.eventManager.subscribe('userListModification', () => this.loadAll());
+  }
+
+  private onSuccess(data, headers) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = headers.get('X-Total-Count');
+    this.users = data;
+  }
+
+  private onError(error) {
+    this.alertService.error(error.error, error.message, null);
+  }
+
+  trackIdentity(index, item: User) {
+    return item.id;
+  }
 }
--- src/main/webapp/app/users/users.route.ts
+++ src/main/webapp/app/users/users.route.ts
@@ -1,14 +1,56 @@
-import { Route } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, Routes } from '@angular/router';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 import { UsersComponent } from './users.component';
+import { User } from 'app/core/user/user.model';
+import { UserService } from 'app/core/user/user.service';
+//import { UsersDetailComponent } from 'app/users/users-detail/users-detail.component';
 
-export const USERS_ROUTE: Route = {
-  path: 'users',
-  component: UsersComponent,
-  data: {
-    authorities: [],
-    pageTitle: 'users.title'
+@Injectable({ providedIn: 'root' })
+export class UsersResolve implements Resolve<any> {
+  constructor(private service: UserService) {}
+
+  resolve(route: ActivatedRouteSnapshot) {
+    const id = route.params['login'] ? route.params['login'] : null;
+    if (id) {
+      return this.service.find(id);
+    }
+    return new User();
+  }
+}
+
+export const USERS_ROUTE: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+    data: {
+      authorities: [],
+      pageTitle: 'users.title'
+    },
+    canActivate: [UserRouteAccessService]
+  }
+
+  /*
+  uncomment when UsersDetail created
+  {
+    path: 'view',
+    component: UsersDetailComponent,
+    resolve: {
+      user: UsersResolve
+    },
+    data: {
+      pageTitle: 'search-users.title'
+    }
   },
-  canActivate: [UserRouteAccessService]
-};
+  {
+    path: ':login/view',
+    component: UsersDetailComponent,
+    resolve: {
+      user: UsersResolve
+    },
+    data: {
+      pageTitle: 'search-users.title'
+    }
+  }*/
+];
